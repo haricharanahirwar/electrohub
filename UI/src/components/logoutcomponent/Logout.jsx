ea95b4f5fc790144c36ae835b3097f81566bb803
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-09-16 12:02:31 +0530

    update

--- UI/src/components/logoutcomponent/Logout.jsx
+++ UI/src/components/logoutcomponent/Logout.jsx
@@ -1,57 +1,59 @@
 import { useEffect } from "react";
 import swal from 'sweetalert2'
-import{useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
 
-function Logout(){
-    const Navigate=useNavigate()
-    const location=useLocation()
-    // useEffect(()=>{
-    //   console.log(location.pathname);
-      
-    // },[])
-    useEffect(()=>{
-        swal.fire({
-            text:'Do you want really to logout',
-            icon:'warning',
-            showCancelButton:true,
-            confirmButtonColor:'yes',
-            cancelButtonText:'No',
-        })
-        .then((result)=>{
-            if(result.isConfirmed)
-            {
-                localStorage.removeItem("token");
-                localStorage.removeItem("email")
-                localStorage.removeItem("name");
-                localStorage.removeItem("mobile");
-                localStorage.removeItem("address");
-                localStorage.removeItem("city");
-                localStorage.removeItem("role");
-                localStorage.removeItem("gender")
-                localStorage.removeItem("info")
-                Navigate("/login");
-            }
-            else{
-                  if(localStorage.getItem("role")=="admin")
-                  {
-                    Navigate("/admin")
-                  }
-                  else if(localStorage.getItem("role")=="user")
-                  {
-                    Navigate("/user")
-                  }
-                  else{
-                    Navigate("/")
-                  }
-            }
+function Logout() {
+  const Navigate = useNavigate()
+  const location = useLocation()
+  // useEffect(()=>{
+  //   console.log(location.pathname);
 
-        })
-    },[location.pathname])
-    return(
-        <>
+  // },[])
+  useEffect(() => {
+    swal.fire({
+      text: 'Do you want really to logout',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: 'yes',
+      cancelButtonText: 'No',
+      confirmButtonColor: "#000000", 
+      cancelButtonColor: "#333333",  
+      background: "#1a1a1a",         
+      color: "#ffffff",              
 
-        </>
-    )
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("email")
+          localStorage.removeItem("name");
+          localStorage.removeItem("mobile");
+          localStorage.removeItem("address");
+          localStorage.removeItem("city");
+          localStorage.removeItem("role");
+          localStorage.removeItem("gender")
+          localStorage.removeItem("info")
+          Navigate("/login");
+        }
+        else {
+          if (localStorage.getItem("role") == "admin") {
+            Navigate("/admin")
+          }
+          else if (localStorage.getItem("role") == "user") {
+            Navigate("/user")
+          }
+          else {
+            Navigate("/")
+          }
+        }
+
+      })
+  }, [location.pathname])
+  return (
+    <>
+
+    </>
+  )
 }
 export default Logout;
--- UI/src/components/usercomponent/User.jsx
+++ UI/src/components/usercomponent/User.jsx
@@ -10,7 +10,7 @@ function User() {
         <div class="row g-5 align-items-center">
             <div class="col-lg-12">
                 <h6 class="section-title text-start text-primary text-uppercase">User Panel</h6>
-                <h1 class="mb-4">Welcome to <span class="text-primary text-uppercase">Shipping War</span></h1>
+                <h1 class="mb-4">Welcome to <span class="text-primary text-uppercase">Electro Hub</span></h1>
             </div>
         </div>
     </div>
